972291c68f786e433bb5224027f7573a7ade3f08
Quadri <kazeemquadri75@gmail.com>
2024-01-24 10:33:40 +0000

    footer update

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
             get<span className="text-textColor">linked</span>
           </h2>
           <p className="text-white font-montserrat pb-2 text-lg text-justify">
-            Getlinked Tech Hackathon is a technology innovation program{" "}
+            Getlinked Tech Hackathon is a technology innovation program
             established by a group of organization with the aim of showcasing
             young and talented individual in the field of technology
           </p>
@@ -23,17 +23,17 @@ const Footer = () => {
             privacy policy
           </a>
         </div>
-        <div className="w-grow">
+        <div className="w-grow font-montserrat">
           <h3 className="font-clash font-semibold text-xl text-textColor pb-2">
             Useful Links
           </h3>
-          <ul className="text-white font-clash cursor-pointer pb-2">
-            <li>Overview</li>
-            <li>Timeline</li>
-            <li>FAQs</li>
-            <li>Register</li>
+          <ul className="text-white  cursor-pointer pb-2">
+            <li className="pb-1">Overview</li>
+            <li className="pb-1">Timeline</li>
+            <li className="pb-1">FAQs</li>
+            <li className="pb-1">Register</li>
           </ul>
-          <div className="flex justify-start items-center gap-2">
+          <div className="flex justify-start items-center my-4 gap-2">
             <p className="text-textColor font-clash font-semibold">Follow Us</p>
             <div className="flex gap-2 text-white cursor-pointer">
               <FaInstagram />
@@ -44,8 +44,10 @@ const Footer = () => {
           </div>
         </div>
         <div className="w-grow">
-          <h3 className="font-clash font-semibold text-textColor text-xl pb-2">Contact Us</h3>
-          <div className="flex justify-start items-center gap-4 text-white font-clash mb-4">
+          <h3 className="font-clash font-semibold text-textColor text-xl pb-2">
+            Contact Us
+          </h3>
+          <div className="flex justify-start items-center gap-4 text-white font-clash mb-6">
             <LuPhoneCall />
             <p>+234 9123456789</p>
           </div>
