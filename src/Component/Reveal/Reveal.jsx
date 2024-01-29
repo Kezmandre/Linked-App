9d0ef4146b362eb09cff281a259f6b470b1080a7
Quadri <kazeemquadri75@gmail.com>
2024-01-29 06:07:25 +0000

    m

--- src/Component/Reveal/Reveal.jsx
+++ src/Component/Reveal/Reveal.jsx
@@ -4,9 +4,9 @@ import {motion} from "framer-motion"
 const Reveal = ({children}) => {
   return (
     <motion.div
-    initial={{opacity:0, scale:0, x:500}}
+    initial={{opacity:0, scale:0, x:-100}}
     whileInView={{opacity:1, scale:1, x:0}}
-    transition={{duration:0.6}}
+    transition={{duration:0.8}}
     viewport={{once:true}}
     
     >{children}</motion.div>
