1bdea762cc526e4c2f5fa0120a2998cf085e2c2a
Quadri <kazeemquadri75@gmail.com>
2024-02-06 12:11:24 +0000

    m

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Contact from '../Component/Contact/Contact'
 import Header from '../Component/Header/Header'
+import Footer from '../Component/Footer/Footer'
 
 const ContactPage = () => {
   return (
     <div>
       <Header/>
       <Contact/>
+      <Footer/>
       </div>
   )
 }
